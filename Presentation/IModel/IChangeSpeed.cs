f24aa3c5ed23b1f9ae7dc1d417007cc96f70091d
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-02 13:10:49 +0300

    Create(Presentation.IModel) several interfaces
    Созданы несколько интерфейсов из тех которые для взаимодействия с Presentation должна релизовать Model: IChangeSpeed, IChangePeopleStrategy, IChangeLiftsStrategy

---
+++ Presentation/IModel/IChangeSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.IModel {
+    interface IChangeSpeed {
+        public DateTime GetTime();
+        public void Stop();
+        public void Start();
+        public void ChangeSpeed(float coefficient);
+
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Presentation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
