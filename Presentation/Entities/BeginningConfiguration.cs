b9df32eaf8b131e763926168dda93f64f4db90b9
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-02 16:29:36 +0300

    Create(Presentation.Entities and IModel) IBeginWithConfiguration
    Создан интерфейс IBeginWithConfiguration для реализации его Моделью и структура BeginningConfiguration для получения Моделью исходных данных (количество этажей и лифтов)

---
+++ Presentation/Entities/BeginningConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.Entities {
+    readonly struct InitialConfiguration {
+        public int QuantityFloors { get; }
+        public int QuantityLifts { get; }
+
+    }
+}
---
+++ Presentation/IModel/IBeginWithConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Presentation.Entities;
+
+namespace Presentation.IModel {
+    interface ISettingInitialConfiguration {
+        public bool Begin(InitialConfiguration configuration);
+
+    }
+}
