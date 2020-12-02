f2456ad15d84bbbebc8b3fea70a83d26fe2db562
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-02 18:36:22 +0300

    Create(P.Entities and P.IModel) IGivingHumanInfo
    Создан интерфейс для Модели, предполагающий передачу информации о обьекте возле заданных координат. Для этого же в enum введен тип Неизвестный. Так же добавлено поле имени для людей.

--- Presentation/Entities/HumanInfo.cs
+++ Presentation/Entities/HumanInfo.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Presentation.Entities {
-    readonly struct HumanStartingData {
+    readonly struct HumanInfo {
 
         public String Name { get; }
         public int StartingFloor { get; }
         public int TargetFloor { get; }
+        public int TravelTime { get; }
+        public int WaitingTime { get; }
+        public bool IsDelivered { get; }
+
     }
 }
---
+++ Presentation/IModel/IGivingHumanInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Presentation.Entities;
+
+namespace Presentation.IModel {
+    interface IGivingHumanInfo {
+        public HumanInfo GetHumanInfo(EntityLocation location);
+    }
+}
