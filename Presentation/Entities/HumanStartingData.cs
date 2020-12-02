c5f9c289affe8699e309f3d1f96c0f402383c478
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-02 15:31:54 +0300

    Create(Presentation.Entities and IModel) IPeopleAddable
    Создан интерфейс IPeopleAddable для реализации его Моделью и структура HumanStartingData для получения Моделью начального этажа и этажа куда человек будет добираться

---
+++ Presentation/Entities/HumanStartingData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.Entities {
+    readonly struct HumanStartingData {
+        public int StartingFloor { get; }
+        public int TargetFloor { get; }
+    }
+}
