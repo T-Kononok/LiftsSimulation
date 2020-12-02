62e4e6c4960c0b3c83bd2fe63ab978beedbf0163
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-02 14:19:26 +0300

    Create(Presentation.Entities) EntityLocation
    Создана структура для передачи информации об положении сущности (лифт или человек) между Model и View через Presentation

--- Presentation/Entities/EntityLocation.cs
+++ Presentation/Entities/EntityLocation.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Presentation.Entities {
-    class EntityLocation {
+
+    readonly struct EntityLocation {
+        public Entities Entities { get; }
+        public int X { get; }
+        public int Y { get; }
+    }
+
+    public enum Entities {
+        Lift,
+        Human
     }
 }
