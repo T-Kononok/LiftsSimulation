a1c17d3696df842dfdd7d9c260eedb109ba5802a
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-03 11:28:45 +0300

    Modify(P.Entity) from Human-structures to classes-heirs abstract Entity-classes
    Структуры для передачи данных связанных с людьми превращены в наследников абстрактных классов данных сущностей. Для того чтобы чуть что добавить других сущностей

--- Presentation/Entities/HumanStartingData.cs
+++ Presentation/Entities/HumanStartingData.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Presentation.Entities {
-    public readonly struct HumanStartingData {
+    public class HumanStartingData : EntityStartingData {
         public String Name { get; }
         public int StartingFloor { get; }
         public int TargetFloor { get; }
 
-        public HumanStartingData(String name, int startingFloor, int targetFloor) {
+        public HumanStartingData(String name, int startingFloor, int targetFloor)
+            : base(EntityType.Human) {
             Name = name;
             StartingFloor = startingFloor;
             TargetFloor = targetFloor;
