0889f6eef5efb64e302621c72edeb0e09e8098ed
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-04 11:50:23 +0300

    Create(PhysicalModel) static class PhysicalContext
    В статическом классе PhysicalContext будут храниться такие характеристики окружения сущностей, как высота и длинна этажа, высота перекрытия между этажами, кол-во самих этажей.

---
+++ PhysicalModel/PhysicalContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicalModel {
+    public static class PhysicalContext {
+
+        public static double FloorsLength { get; } = 50.0;
+        public static double FloorsHeight { get; } = 3.0;
+        public static double SlabsHeight { get; } = 0.5;
+
+        private static int _quantityFloors = 2;
+        public static int QuantityFloors {
+            get {
+                return _quantityFloors;
+            }
+        }
+
+        public static bool SetQuantityFloors(int quantity) {
+            if (QuantityFloors > 2)
+                return false;
+            _quantityFloors = quantity;
+            return true;
+        }
+    }
+}
