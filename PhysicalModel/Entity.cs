804e509687fd92c024ab2096878c161b0208de9d
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-04 09:47:36 +0300

    Create(PhysicalModel) abstract Entity
    Создан абстрактный класс Сущности, от которого будут наследовать всяческие лифты, люди и тд.

---
+++ PhysicalModel/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Presentation.Entities;
+
+namespace PhysicalModel {
+    public abstract class Entity {
+        public String Name { get; } = "";
+        public EntityType Type { get; } = EntityType.Unknown;
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+
+        public Entity(String name, EntityType type, int x, int y) {
+            Name = name;
+            Type = type;
+            X = x;
+            Y = y;
+        }
+
+        public abstract void RecalculateLocation();
+
+        public abstract EntityLocation GetLocation();
+    }
+}
