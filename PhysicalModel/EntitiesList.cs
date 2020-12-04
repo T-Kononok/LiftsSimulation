601feb8601d96d2b2c91b505a5bfaf6e934b7976
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-04 09:56:37 +0300

    Create(PhysicalModel) EntitiesList to store the created entity
    В EntitiesList хранятся все сущности (лифты, люди и тд). Главная цель EntitiesList в том, чтобы сущности каждый такт пересчитывали свое местоположение и формировали список новых координат для передачи в Presentation.

---
+++ PhysicalModel/EntitiesList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Entities;
+
+namespace PhysicalModel {
+    class EntitiesList {
+
+        private List<Entity> _entities = new List<Entity>();
+        private List<EntityLocation> _locations;
+
+        public delegate void LocationsChangedHandler(List<EntityLocation> locations);
+        event LocationsChangedHandler LocationsChanged;
+
+        public void SetLocationsChangedHandler(LocationsChangedHandler handler) {
+            LocationsChanged += handler;
+        }
+
+        private void ClockHandler() {
+            _locations = new List<EntityLocation>();
+            Parallel.ForEach(_entities, RecalculateLocation);
+            LocationsChanged(_locations);
+        }
+
+        void RecalculateLocation(Entity entity) {
+            entity.RecalculateLocation();
+            _locations.Add(entity.GetLocation());
+        }
+
+        public EntitiesList(ClockGenerator generator) {
+            generator.SetClockHandler(ClockHandler);
+        }
+
+        public void AddEntity(Entity data) {
+            //
+        }
+    }
+}
