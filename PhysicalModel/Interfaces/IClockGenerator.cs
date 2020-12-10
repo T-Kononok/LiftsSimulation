ff9ccc6589c2f2bccef0ea388e35240b2ceaab7c
Кононок Тимофей Сергеевич <57789444+T-Kononok@users.noreply.github.com>
2020-12-10 14:50:15 +0300

    Create(PhysicalModel) IClockGenerator
    Создан интерфейс тактового генератора, который будет моделировать течение времени.

---
+++ PhysicalModel/Interfaces/IClockGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicalModel.Interfaces {
+    interface IClockGenerator {
+
+        double Сoefficient { get; set; }
+
+        DateTime Time { get; }
+
+        delegate void ClockHandler();
+        void SetClockHandler(ClockHandler handler);
+
+        void Start();
+        void Stop();
+
+        void Pause();
+        void Play();
+    }
+}
